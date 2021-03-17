79b2db3cc9b4ee3583895f744ee03b488b622d79
mstfhlc <mustafahalici30@gmail.com>
2021-03-17 13:16:00 +0100

    creating

---
+++ ex01/src/index.js
@@ -0,0 +1,25 @@
+// Only change code below this line
+
+var myPet={
+    species:"animal",
+    name:"dog",
+    legs:4,
+    friends:[
+        "cat","bird"
+    ]
+    
+
+}
+
+function myFunction(myobj){
+    return myobj;
+}
+
+
+
+
+
+
+// Only change code below this line
+console.log(myFunction(myPet));//Change this line
+module.exports={myPet,myFunction};
